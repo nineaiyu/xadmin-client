12f8b39abc74d84f58f8bddc61809b739065e54d
isummer <nineven@qq.com>
2023-07-04 11:45:29 +0800

    fix: 登录图片验证码不显示问题

--- README.md
+++ README.md
@@ -11,3 +11,19 @@ Django 做后端服务
 
 [https://xadmin.dvcloud.xin/](https://xadmin.dvcloud.xin/)
 账号密码：admin/admin123
+
+## docker 构建
+
+修改 api 配置文件`.env.production`，将 api 域名修改为自己服务器，然后进行构建
+
+```shell
+docker compose up xadmin-client-build
+```
+
+### docker 启动
+
+```shell
+docker compose up xadmin-client-prod
+```
+
+然后浏览器 http://localhost:8891 进行访问
--- src/components/ReImageVerify/src/hooks.ts
+++ src/components/ReImageVerify/src/hooks.ts
@@ -6,10 +6,21 @@ export const useImageVerify = () => {
   const imgCode = ref("");
   const imgUrl = ref("");
 
+  const apiDomain = res => {
+    const httpReg = /^http(s?):\/\//;
+    if (
+      import.meta.env.VITE_API_DOMAIN &&
+      httpReg.test(import.meta.env.VITE_API_DOMAIN)
+    ) {
+      return `${import.meta.env.VITE_API_DOMAIN}${res.captcha_image}`;
+    }
+    return res.captcha_image;
+  };
+
   function getImgCode() {
     getCaptchaApi().then(res => {
       if (res.code === 1000) {
-        imgUrl.value = res.captcha_image;
+        imgUrl.value = apiDomain(res);
         imgCode.value = res.captcha_key;
         useUserStoreHook().SET_VERIFY_CODE_LENGTH(res.length);
       }
