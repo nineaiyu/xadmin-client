546d02fea3bed0f64e1550812849dbffa260ef5f
isummer <nineven@qq.com>
2023-09-16 13:50:40 +0800

    fix: 修复编辑器无法打开

--- src/utils/preventDefault.ts
+++ src/utils/preventDefault.ts
@@ -18,7 +18,7 @@ export const addPreventDefault = () => {
   // 阻止浏览器默认的右键菜单弹出（不会影响自定义右键事件）
   useEventListener(window.document, "contextmenu", ev => ev.preventDefault());
   // 阻止页面元素选中
-  useEventListener(window.document, "selectstart", ev => ev.preventDefault());
+  // useEventListener(window.document, "selectstart", ev => ev.preventDefault());
   // 浏览器中图片通常默认是可拖动的，并且可以在新标签页或窗口中打开，或者将其拖动到其他应用程序中，此处将其禁用，使其默认不可拖动
   useEventListener(
     window.document,
