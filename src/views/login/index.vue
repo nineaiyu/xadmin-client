e825197feb443e6f7678b7206fb25205ec48bd83
isummer <nineven@qq.com>
2024-07-10 16:07:24 +0800

    feat: 增加登录注册验证码校验，优化登录流程

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,178 +1,39 @@
 <script lang="ts" setup>
-import {
-  computed,
-  onBeforeUnmount,
-  onMounted,
-  reactive,
-  ref,
-  toRaw,
-  watch
-} from "vue";
-import { useI18n } from "vue-i18n";
+import { computed, toRaw } from "vue";
 import Motion from "./utils/motion";
-import { useRouter } from "vue-router";
-import { message } from "@/utils/message";
-import { loginRules } from "./utils/rule";
 import phone from "./components/phone.vue";
 import TypeIt from "@/components/ReTypeit";
 import qrCode from "./components/qrCode.vue";
 import register from "./components/register.vue";
 import update from "./components/update.vue";
+import login from "./components/login.vue";
 import { useNav } from "@/layout/hooks/useNav";
-import type { FormInstance } from "element-plus";
-import { $t, transformI18n } from "@/plugins/i18n";
-import { operates, thirdParty } from "./utils/enums";
 import { useLayout } from "@/layout/hooks/useLayout";
 import { useUserStoreHook } from "@/store/modules/user";
-import { getTopMenu, initRouter } from "@/router/utils";
 import { avatar, bg, illustration } from "./utils/static";
-import { ReImageVerify } from "@/components/ReImageVerify";
-import { useRenderIcon } from "@/components/ReIcon/src/hooks";
+
 import { useTranslationLang } from "@/layout/hooks/useTranslationLang";
 import { useDataThemeChange } from "@/layout/hooks/useDataThemeChange";
 
 import dayIcon from "@/assets/svg/day.svg?component";
 import darkIcon from "@/assets/svg/dark.svg?component";
 import globalization from "@/assets/svg/globalization.svg?component";
-import Lock from "@iconify-icons/ri/lock-fill";
 import Check from "@iconify-icons/ep/check";
-import User from "@iconify-icons/ri/user-3-fill";
-import Info from "@iconify-icons/ri/information-line";
-import { getTempTokenApi } from "@/api/auth";
-import { cloneDeep, debounce } from "@pureadmin/utils";
-import { useEventListener } from "@vueuse/core";
 
 defineOptions({
-  name: "Login"
+  name: "LoginIndex"
 });
 
-const router = useRouter();
-const loading = ref(false);
-const checked = ref(true);
-const disabled = ref(false);
-const loginDay = ref(1);
-const loginDayList = ref([1]);
-const ruleFormRef = ref<FormInstance>();
 const currentPage = computed(() => {
   return useUserStoreHook().currentPage;
 });
 
-const { t } = useI18n();
 const { initStorage } = useLayout();
 initStorage();
 const { dataTheme, overallStyle, dataThemeChange } = useDataThemeChange();
 dataThemeChange(overallStyle.value);
 const { title, getDropdownItemStyle, getDropdownItemClass } = useNav();
 const { locale, translationCh, translationEn } = useTranslationLang();
-
-const ruleForm = reactive({
-  username: "",
-  password: "",
-  token: "",
-  captcha_key: "",
-  captcha_code: ""
-});
-
-const formatLoginDayList = () => {
-  const start = 1;
-  const middle = Math.ceil(loginDay.value / 2);
-  if (middle > 0) {
-    if (middle !== start) {
-      loginDayList.value.push(middle);
-    }
-    if (middle !== loginDay.value) {
-      loginDayList.value.push(loginDay.value);
-    }
-  } else {
-    loginDayList.value = [loginDay.value];
-  }
-};
-
-const initToken = () => {
-  getTempTokenApi().then(res => {
-    if (res.code === 1000) {
-      ruleForm.token = res.token;
-      loginDay.value = res.lifetime;
-      formatLoginDayList();
-    }
-  });
-};
-
-const onLogin = async (formEl: FormInstance | undefined) => {
-  if (!formEl) return;
-  await formEl.validate(valid => {
-    if (valid) {
-      loading.value = true;
-      useUserStoreHook()
-        .loginByUsername(cloneDeep(ruleForm))
-        .then(res => {
-          if (res.code === 1000) {
-            message(transformI18n($t("login.loginSuccess")), {
-              type: "success"
-            });
-            initRouter().then(() => {
-              disabled.value = true;
-              router.push(getTopMenu(true).path).finally(() => {
-                disabled.value = false;
-              });
-            });
-            loading.value = false;
-          } else {
-            message(res.detail, {
-              type: "warning"
-            });
-          }
-        })
-        .catch(err => {
-          message(err.detail, {
-            type: "warning"
-          });
-          loading.value = false;
-          initToken();
-        })
-        .finally(() => (loading.value = false));
-    } else {
-      loading.value = false;
-    }
-  });
-};
-
-const immediateDebounce: any = debounce(
-  formRef => onLogin(formRef),
-  1000,
-  true
-);
-
-/** 使用公共函数，避免`removeEventListener`失效 */
-function onkeypress({ code }: KeyboardEvent) {
-  if (code === "Enter" || code === "NumpadEnter") {
-    onLogin(ruleFormRef.value);
-  }
-}
-
-onMounted(() => {
-  window.document.addEventListener("keypress", onkeypress);
-  initToken();
-  useUserStoreHook().SET_ISREMEMBERED(checked.value);
-  useUserStoreHook().SET_LOGINDAY(loginDay.value);
-});
-
-onBeforeUnmount(() => {
-  useEventListener(document, "keypress", ({ code }) => {
-    if (
-      ["Enter", "NumpadEnter"].includes(code) &&
-      !disabled.value &&
-      !loading.value
-    )
-      immediateDebounce(ruleFormRef.value);
-  });
-});
-watch(checked, bool => {
-  useUserStoreHook().SET_ISREMEMBERED(bool);
-});
-watch(loginDay, value => {
-  useUserStoreHook().SET_LOGINDAY(value);
-});
 </script>
 
 <template>
@@ -235,151 +96,8 @@ watch(loginDay, value => {
             </h2>
           </Motion>
 
-          <el-form
-            v-if="currentPage === 0"
-            ref="ruleFormRef"
-            :model="ruleForm"
-            :rules="loginRules"
-            size="large"
-          >
-            <Motion :delay="100">
-              <el-form-item
-                :rules="[
-                  {
-                    required: true,
-                    message: transformI18n($t('login.usernameReg')),
-                    trigger: 'blur'
-                  }
-                ]"
-                prop="username"
-              >
-                <el-input
-                  v-model="ruleForm.username"
-                  :placeholder="t('login.username')"
-                  :prefix-icon="useRenderIcon(User)"
-                  clearable
-                />
-              </el-form-item>
-            </Motion>
+          <login v-if="currentPage === 0" />
 
-            <Motion :delay="150">
-              <el-form-item prop="password">
-                <el-input
-                  v-model="ruleForm.password"
-                  :placeholder="t('login.password')"
-                  :prefix-icon="useRenderIcon(Lock)"
-                  clearable
-                  show-password
-                />
-              </el-form-item>
-            </Motion>
-
-            <Motion :delay="200">
-              <el-form-item prop="captcha_code">
-                <el-input
-                  v-model="ruleForm.captcha_code"
-                  :placeholder="t('login.verifyCode')"
-                  :prefix-icon="useRenderIcon('ri:shield-keyhole-line')"
-                  clearable
-                >
-                  <template v-slot:append>
-                    <ReImageVerify v-model="ruleForm.captcha_key" />
-                  </template>
-                </el-input>
-              </el-form-item>
-            </Motion>
-
-            <Motion :delay="250">
-              <el-form-item>
-                <div class="w-full h-[20px] flex justify-between items-center">
-                  <el-checkbox v-model="checked">
-                    <span class="flex">
-                      <select
-                        v-model="loginDay"
-                        :disabled="loginDayList.length < 2"
-                        :style="{
-                          width: loginDay < 10 ? '10px' : '16px',
-                          outline: 'none',
-                          background: 'none',
-                          appearance: 'none'
-                        }"
-                      >
-                        <option
-                          v-for="item in loginDayList"
-                          :key="item"
-                          :value="item"
-                        >
-                          {{ item }}
-                        </option>
-                      </select>
-                      {{ t("login.remember") }}
-                      <el-tooltip
-                        :content="t('login.rememberInfo')"
-                        effect="dark"
-                        placement="top"
-                      >
-                        <IconifyIconOffline :icon="Info" class="ml-1" />
-                      </el-tooltip>
-                    </span>
-                  </el-checkbox>
-                  <el-button
-                    link
-                    type="primary"
-                    @click="useUserStoreHook().SET_CURRENT_PAGE(4)"
-                  >
-                    {{ t("login.forget") }}
-                  </el-button>
-                </div>
-                <el-button
-                  :disabled="disabled"
-                  :loading="loading"
-                  class="w-full mt-4"
-                  size="default"
-                  type="primary"
-                  @click="onLogin(ruleFormRef)"
-                >
-                  {{ t("login.login") }}
-                </el-button>
-              </el-form-item>
-            </Motion>
-
-            <Motion :delay="300">
-              <el-form-item>
-                <div class="w-full h-[20px] flex justify-between items-center">
-                  <el-button
-                    v-for="(item, index) in operates"
-                    :key="index"
-                    class="w-full mt-4"
-                    size="default"
-                    @click="useUserStoreHook().SET_CURRENT_PAGE(index + 1)"
-                  >
-                    {{ t(item.title) }}
-                  </el-button>
-                </div>
-              </el-form-item>
-            </Motion>
-          </el-form>
-
-          <Motion v-if="currentPage === 0" :delay="350">
-            <el-form-item>
-              <el-divider>
-                <p class="text-gray-500 text-xs">{{ t("login.thirdLogin") }}</p>
-              </el-divider>
-              <div class="w-full flex justify-evenly">
-                <span
-                  v-for="(item, index) in thirdParty"
-                  :key="index"
-                  :title="t(item.title)"
-                >
-                  <IconifyIconOnline
-                    :icon="`ri:${item.icon}-fill`"
-                    class="cursor-pointer text-gray-500 hover:text-blue-400"
-                    width="20"
-                  />
-                </span>
-              </div>
-            </el-form-item>
-          </Motion>
           <!-- 手机号登录 -->
           <phone v-if="currentPage === 1" />
           <!-- 二维码登录 -->
