01d4b0718442c24544736afc22d82c5f1b8f4d91
isummer <nineven@qq.com>
2024-07-19 20:28:35 +0800

    refactor: 重构page组件

--- src/components/ReBaseTable/index.ts
+++
@@ -1,7 +0,0 @@
-import reBaseTable from "./src/index.vue";
-import { withInstall } from "@pureadmin/utils";
-
-/** 图片裁剪预览组件 */
-export const ReBaseTable = withInstall(reBaseTable);
-
-export default ReBaseTable;
--- src/components/ReBaseTable/src/form/constants.ts
+++
@@ -1,4 +0,0 @@
-export const FormatOptions = [
-  { label: "CSV", value: "csv" },
-  { label: "Excel", value: "xlsx" }
-];
