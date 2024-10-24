d6a798a9f3ab8ec5e854a022af9e5fbf29fadaf2
isummer <nineven@qq.com>
2024-10-24 13:32:02 +0800

    perf: 容器镜像默认从华为云拉取

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-#
-#
+
+DOCKER_IMAGE_PREFIX="swr.cn-north-4.myhuaweicloud.com/nineaiyu"
+
+images="node:22.10.0-alpine xadmin-node:22.10.0-alpine"
+for image in ${images};do
+  full_image_path="${DOCKER_IMAGE_PREFIX}/${image}"
+  docker pull  "${full_image_path}"
+  docker tag "${full_image_path}" "${image}"
+  docker rmi -f "${full_image_path}"
+done
+
 # clean old build docker and image
 docker rm -f xadmin-client-build
 docker image rm xadmin-client-build
