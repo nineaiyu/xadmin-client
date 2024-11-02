ae4805a6b31a8a40ad52788fb54e3b723fbcc61a
isummer <nineven@qq.com>
2024-11-02 12:33:19 +0800

    perf: 优化注释

--- src/views/demo/book/index.vue
+++ src/views/demo/book/index.vue
@@ -1,8 +1,9 @@
 <script lang="ts" setup>
 import { RePlusPage } from "@/components/RePlusPage";
 import { useDemoBook } from "./utils/hook";
+
 defineOptions({
-  name: "DemoBook"
+  name: "DemoBook" // 必须定义，用于菜单自动匹配组件
 });
 const { api, auth } = useDemoBook();
 </script>
