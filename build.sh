c412ad6f2ab60b5ab988e65a580b8563999b4797
isummer <nineven@qq.com>
2024-11-18 17:59:51 +0800

    perf: 优化本地构建脚本

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+node_images="registry.cn-beijing.aliyuncs.com/nineaiyu/node:22.11.0-slim"
+
+npm_mirror="https://registry.npmmirror.com"
+
+cmd='corepack enable && corepack prepare pnpm@9.12.3 --activate \
+    && cd /app && pnpm install --frozen-lockfile && pnpm build'
+
 docker run --rm -it -v ./:/app -e TZ=Asia/Shanghai \
-  nineaiyu/xadmin-client-base:20241117_124931 sh -c 'pnpm install --frozen-lockfile && pnpm build'
+    -e COREPACK_NPM_REGISTRY=${npm_mirror} \
+    -e npm_config_registry=${npm_mirror} \
+    ${node_images} sh -c "${cmd}"
 
