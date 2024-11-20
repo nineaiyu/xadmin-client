51d397ae016de1e95fd718bca584ad0b050fed9c
isummer <nineven@qq.com>
2024-11-20 14:41:27 +0800

    feat: 增加手动构建版本传参

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+
+VERSION=$1
 
 node_images="registry.cn-beijing.aliyuncs.com/nineaiyu/node:22.11.0-slim"
 
@@ -6,6 +9,11 @@ npm_mirror="https://registry.npmmirror.com"
 
 cmd='corepack enable && corepack prepare pnpm@9.12.3 --activate \
     && cd /app && pnpm install --frozen-lockfile && pnpm build'
+
+if [[ -n ${VERSION} ]]; then
+sed -i "s@\"Version\": .*@\"Version\": \"${VERSION/v/}\",@" public/platform-config.json  \
+    && sed -i "s@\"version\": .*@\"version\": \"${VERSION/v/}\",@" package.json
+fi
 
 docker run --rm -it -v ./:/app -e TZ=Asia/Shanghai \
     -e COREPACK_NPM_REGISTRY=${npm_mirror} \
