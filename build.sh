092ee7f4dd875adfa19d2bd508e7b1aa52583cad
isummer <nineven@qq.com>
2025-02-21 16:39:04 +0800

    fix: 修改头像跨域异常

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ node_images="registry.cn-beijing.aliyuncs.com/nineaiyu/node:22.14.0-slim"
 
 npm_mirror="https://registry.npmmirror.com"
 
-cmd='corepack enable && corepack prepare pnpm@9.14.2 --activate \
+cmd='corepack enable && corepack prepare pnpm@10.4.1 --activate \
     && cd /app && pnpm install --frozen-lockfile && pnpm build'
 
 if [[ -n ${VERSION} ]]; then
