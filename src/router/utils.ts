7b220faeb19b4702986ece59bdc51b770583aec2
isummer <nineven@qq.com>
2025-09-09 11:06:31 +0800

    fix: 修复外链跳转异常

--- src/router/utils.ts
+++ src/router/utils.ts
@@ -13,6 +13,7 @@ import {
   intersection,
   isAllEmpty,
   isObject,
+  isUrl,
   storageLocal
 } from "@pureadmin/utils";
 import { getConfig } from "@/config";
@@ -319,8 +320,14 @@ function addAsyncRoutes(arrRoutes: Array<RouteRecordRaw>) {
     if (v?.children && v.children.length && !v.redirect)
       v.redirect = v.children[0].path;
     // 父级的name属性取值：如果子级存在且父级的name属性不存在，默认取第一个子级的name；如果子级存在且父级的name属性存在，取存在的name属性，会覆盖默认值（注意：测试中发现父级的name不能和子级name重复，如果重复会造成重定向无效（跳转404），所以这里给父级的name起名的时候后面会自动加上`Parent`，避免重复）
-    if (v?.children && v.children.length && !v.name)
-      v.name = (v.children[0].name as string) + "Parent";
+    if (v?.children && v.children.length && !v.name) {
+      if (isUrl(v.children[0].name as string)) {
+        v.name = v.children[0].name as string;
+        v.children[0].name = v.name + "Child";
+      } else {
+        v.name = (v.children[0].name as string) + "Parent";
+      }
+    }
     if (v.meta?.frameSrc) {
       v.component = IFrame;
     } else {
